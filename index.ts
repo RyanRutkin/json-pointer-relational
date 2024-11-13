5637f2fda7f9a2336a458e21f5aec6506a126c5d
rutkin.ryan@gmail.com <rutkin.ryan@gmail.com>
2024-11-13 16:56:34 -0600

    Now exporting tokenizeJsonPointer

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import { getReferenceByPointer, getByPointer, setByPointer, setByPointerWithRef, RefPoint as RefPointDef } from './json-pointer-relational';
+import {
+    getReferenceByPointer,
+    getByPointer, setByPointer,
+    setByPointerWithRef,
+    tokenizeJsonPointer,
+    RefPoint as RefPointDef
+} from './json-pointer-relational';
 export {
     getReferenceByPointer,
     getByPointer,
     setByPointer,
-    setByPointerWithRef
+    setByPointerWithRef,
+    tokenizeJsonPointer
 };
 export type RefPoint = RefPointDef;
